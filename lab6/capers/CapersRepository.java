ea860e3c28299154b34b16bad128c2ea1a1807e4
jalvlue <1722292195@qq.com>
2024-01-31 22:51:31 +0800

    lab6 completed.

--- lab6/capers/CapersRepository.java
+++ lab6/capers/CapersRepository.java
@@ -1,46 +1,82 @@
 package capers;
 
 import java.io.File;
+import java.io.IOException;
+
 import static capers.Utils.*;
 
-/** A repository for Capers 
- * @author TODO
+/**
+ * A repository for Capers
+ *
+ * @author jalvlue
  * The structure of a Capers Repository is as follows:
- *
+ * <p>
  * .capers/ -- top level folder for all persistent data in your lab12 folder
- *    - dogs/ -- folder containing all of the persistent data for dogs
- *    - story -- file containing the current story
- *
- * TODO: change the above structure if you do something different.
+ * - dogs/ -- folder containing all of the persistent data for dogs
+ * - story -- file containing the current story
+ * <p>
  */
 public class CapersRepository {
-    /** Current Working Directory. */
+    /**
+     * Current Working Directory.
+     */
     static final File CWD = new File(System.getProperty("user.dir"));
 
-    /** Main metadata folder. */
-    static final File CAPERS_FOLDER = null; // TODO Hint: look at the `join`
-                                            //      function in Utils
+    /**
+     * Main metadata folder.
+     */
+    static final File CAPERS_FOLDER = join(CapersRepository.CWD, ".capers");
 
     /**
      * Does required filesystem operations to allow for persistence.
      * (creates any necessary folders or files)
      * Remember: recommended structure (you do not have to follow):
-     *
+     * <p>
      * .capers/ -- top level folder for all persistent data in your lab12 folder
-     *    - dogs/ -- folder containing all of the persistent data for dogs
-     *    - story -- file containing the current story
+     * - dogs/ -- folder containing all of the persistent data for dogs
+     * - story -- file containing the current story
      */
     public static void setupPersistence() {
-        // TODO
+        File dotCapers = CapersRepository.CAPERS_FOLDER;
+        if (!dotCapers.exists()) {
+            if (dotCapers.mkdir()) {
+//                System.out.printf("dir created: %s\n", dotCapers.getAbsoluteFile());
+            }
+        }
+
+        File dogs = Dog.DOG_FOLDER;
+        if (!dogs.exists()) {
+            if (dogs.mkdir()) {
+//                System.out.printf("dir created: %s\n", dogs.getAbsoluteFile());
+            }
+        }
+
+        File story = join(CapersRepository.CAPERS_FOLDER, "story");
+        if (!story.exists()) {
+            try {
+                if (story.createNewFile()) {
+//                    System.out.printf("file created: %s\n", story.getAbsoluteFile());
+                }
+            } catch (IOException e) {
+                System.out.printf("error {%s} occurred while creating file {%s}\n", e.getMessage(), story.getAbsoluteFile());
+            }
+        }
     }
 
     /**
      * Appends the first non-command argument in args
      * to a file called `story` in the .capers directory.
+     *
      * @param text String of the text to be appended to the story
      */
     public static void writeStory(String text) {
-        // TODO
+        File story = join(CapersRepository.CWD, ".capers", "story");
+
+        String newContent = readContentsAsString(story) + text + "\n";
+        System.out.println(newContent);
+
+        writeContents(story, newContent);
+
     }
 
     /**
@@ -49,16 +85,22 @@ public class CapersRepository {
      * Also prints out the dog's information using toString().
      */
     public static void makeDog(String name, String breed, int age) {
-        // TODO
+        Dog savedDog = new Dog(name, breed, age);
+
+        savedDog.saveDog();
+        System.out.println(savedDog);
     }
 
     /**
      * Advances a dog's age persistently and prints out a celebratory message.
      * Also prints out the dog's information using toString().
      * Chooses dog to advance based on the first non-command argument of args.
+     *
      * @param name String name of the Dog whose birthday we're celebrating.
      */
     public static void celebrateBirthday(String name) {
-        // TODO
+        Dog celeDog = Dog.fromFile(name);
+        celeDog.haveBirthday();
+        celeDog.saveDog();
     }
 }
