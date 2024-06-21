43f5eef87bcde13b2910ab1ad93ae272f9ecf911
jalvlue <1722292195@qq.com>
2024-06-21 23:31:32 +0800

    lab3 completed

--- lab3/randomizedtest/BuggyAList.java
+++ lab3/randomizedtest/BuggyAList.java
@@ -2,7 +2,7 @@ package randomizedtest;
 
 /**
  * Array based list.
- * 
+ *
  * @author Josh Hug
  */
 
@@ -21,13 +21,17 @@ public class BuggyAList<Item> {
     private Item[] items;
     private int size;
 
-    /** Creates an empty list. */
+    /**
+     * Creates an empty list.
+     */
     public BuggyAList() {
         items = (Item[]) new Object[1];
         size = 0;
     }
 
-    /** Resizes the underlying array to the target capacity. */
+    /**
+     * Resizes the underlying array to the target capacity.
+     */
     private void resize(int capacity) {
         Item[] a = (Item[]) new Object[capacity];
         for (int i = 0; i < size; i += 1) {
@@ -36,7 +40,9 @@ public class BuggyAList<Item> {
         items = a;
     }
 
-    /** Inserts X into the back of the list. */
+    /**
+     * Inserts X into the back of the list.
+     */
     public void addLast(Item x) {
         if (size == items.length) {
             resize(size * 2);
@@ -45,17 +51,23 @@ public class BuggyAList<Item> {
         size = size + 1;
     }
 
-    /** Returns the item from the back of the list. */
+    /**
+     * Returns the item from the back of the list.
+     */
     public Item getLast() {
         return items[size - 1];
     }
 
-    /** Gets the ith item in the list (0 is the front). */
+    /**
+     * Gets the ith item in the list (0 is the front).
+     */
     public Item get(int i) {
         return items[i];
     }
 
-    /** Returns the number of items in the list. */
+    /**
+     * Returns the number of items in the list.
+     */
     public int size() {
         return size;
     }
